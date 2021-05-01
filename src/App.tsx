555245f5a96915b7d0501133751d32cbcf1b199a
OmarMWarraich <earthling986@gmail.com>
2021-05-01 08:16:37 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import bellyfloat from './assets/bellyfloat.png';
+import starlinkdish from './assets/starlinkdish.png';
+import starship1 from './assets/starship1.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  render() {
+      return (
+        <div className="demo-container">
+          <Carousel className="demo-carousel">
+              <div>
+                  <img src={bellyfloat} alt='1' />
+                  <p className="legend">Legend 1</p>
+              </div>
+              <div>
+                  <img src={starlinkdish} alt='2'/>
+                  <p className="legend">Legend 2</p>
+              </div>
+              <div>
+                  <img src={starship1} alt='3'/>
+                  <p className="legend">Legend 3</p>
+              </div>
+          </Carousel>
+          </div>
+      );
+  }
+};
 export default App;
