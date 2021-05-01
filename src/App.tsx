07e5a47f8e8b6330f3d69a645c3d9eb2047c30cf
OmarMWarraich <earthling986@gmail.com>
2021-05-01 09:09:22 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
-import React, { Component } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import bellyfloat from './assets/bellyfloat.png';
-import starlinkdish from './assets/starlinkdish.png';
-import starship1 from './assets/starship1.png';
+import React from 'react';
+import styled from "styled-components";
 
+import Carousel from "./components/Carousel";
+import SlideOne from './components/carouselSlides/SlideOne';
+import SlideTwo from './components/carouselSlides/SlideTwo';
+import SlideThree from './components/carouselSlides/SlideThree';
 
-class App extends Component {
-  render() {
-      return (
-        <div className="demo-container">
-          <Carousel className="demo-carousel">
-              <div>
-                  <img src={bellyfloat} alt='1' />
-                  <p className="legend">Legend 1</p>
-              </div>
-              <div>
-                  <img src={starlinkdish} alt='2'/>
-                  <p className="legend">Legend 2</p>
-              </div>
-              <div>
-                  <img src={starship1} alt='3'/>
-                  <p className="legend">Legend 3</p>
-              </div>
-          </Carousel>
-          </div>
-      );
-  }
-};
+const App: React.FC = () => {
+  return (
+    <div>
+      <Carousel>
+        <SlideOne />
+        <SlideTwo />
+        <SlideThree />
+      </Carousel>
+    </div>
+  );
+}
+
 export default App;
